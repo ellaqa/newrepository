98791f0d524470dc4591436cde0961dc1e3ed5d5
ellaqa <49357916+ellaqa@users.noreply.github.com>
2020-12-10 22:27:59 -0800

    Add files via upload

---
+++ triangle_two.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+script, a, b, c = argv
+
+if a + b <= c or a + c <= b or b + c <= a:
+    print("Triangle is not existent")
+elif a != b and a != c and b != c:
+    print ("The triangle is scalene")
+elif a == b == c:
+    print ("The triangle is equilateral ")
+else:
+    print ("The triangle is isosceles")
