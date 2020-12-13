0d60efe714adda17c233c46a6758267bfc9eb572
ellaqa <49357916+ellaqa@users.noreply.github.com>
2020-12-12 21:28:15 -0800

    Update triangle_two.py

--- triangle_two.py
+++ triangle_two.py
@@ -1,12 +1,15 @@
 from sys import argv
-
 script, a, b, c = argv
 
-if a + b <= c or a + c <= b or b + c <= a:
-    print("Triangle is not existent")
-elif a != b and a != c and b != c:
-    print ("The triangle is scalene")
-elif a == b == c:
-    print ("The triangle is equilateral ")
-else:
-    print ("The triangle is isosceles")
+#@pytest.fixture
+
+
+def triangle(a,b,c):
+    if a + b <= c or a + c <= b or b + c <= a:
+        return 'not_existent'
+    elif a != b and a != c and b != c:
+        return 'scalene'
+    elif a == b == c:
+        return 'equilateral'
+    else:
+        return 'isosceles'
